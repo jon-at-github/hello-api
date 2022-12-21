485e30d7297676cc6dee8454ce14e957c7f6b471
jon <kuxtal.kiimil@gmail.com>
2022-12-21 10:59:24 -0600

    - implemented faas deployment step

---
+++ faas.go
@@ -0,0 +1,11 @@
+package faas
+
+import (
+	"net/http"
+
+	"github.com/jon-at-github/hello-api/handlers/rest"
+)
+
+func Translate(w http.ResponseWriter, r *http.Request) {
+	rest.TranslateHandler(w, r)
+}
