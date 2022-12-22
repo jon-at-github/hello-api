cbf4c55794ddce1b9c340eeb0d00daa0d4d1f26a
jon <kuxtal.kiimil@gmail.com>
2022-12-21 19:38:47 -0600

    - added paas deployment and health check

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/jon-at-github/hello-api/handlers"
 	"github.com/jon-at-github/hello-api/handlers/rest"
 )
 
 func main() {
-	addr := ":8080"
+	addr := fmt.Sprintf(":%s", os.Getenv("PORT"))
+	if addr == ":" {
+		addr = ":8080"
+	}
 
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/hello", rest.TranslateHandler)
+	mux.HandleFunc("/translate/hello", rest.TranslateHandler)
+	mux.HandleFunc("/health", handlers.HealthCheck)
 
 	log.Printf("listeting on %s\n", addr)
 
